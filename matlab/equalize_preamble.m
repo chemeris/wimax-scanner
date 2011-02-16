291bf6ae75a0b04fbcb09da1b9e7f1e2eab9cbd5
Alexander Chemeris <chemeris@users.noreply.github.com>
2011-02-16 11:59:33 +0300

    Matlab: Rename equalize.m to equalize_preamble.m

--- matlab/equalize_preamble.m
+++ matlab/equalize_preamble.m
@@ -16,25 +16,43 @@
 % Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
 % USA
 
-%PN_index = 11; %  Preamble Index
-sp_ref = preamble_freq(params.preamble_idx+1,:); % Ideal pilot signal
+%% Get ideal signal
+sp_ref = preamble_freq_shifted(params.preamble_idx+1,:); % Ideal pilot signal
 X = find(sp_ref ~= 0); % индексы поднесущих пилот-сигнала
 sp_ref = sp_ref(sp_ref ~= 0); 
 
 %% Get received signal
-sig_in = fft(rcvdDL(theta:theta+params.Tb_samples-1))'; % Received pilot signal
+sig_in = fftshift(fft(rcvdDL(theta:theta+params.Tb_samples-1))'); % Received pilot signal
 sp_rx = sig_in(X); % Received pilot signal
  
 %% Calculate erorror over pilot signals
 sp_err = sp_ref ./ sp_rx; 
  
 %% Interpolate error voctor over all sub-carriers
-err_vec = interp1(X, sp_err, 1:length(sig_in), 'spline'); 
+err_vec_preamble = interp1(X, sp_err, 1:length(sig_in), 'spline'); 
  
 %% Correct signal
-rx_sym = sig_in .* err_vec;
+rx_sym = sig_in .* err_vec_preamble;
  
 %% Do plots
-figure; hold on ; plot(abs(fftshift(sig_in))); plot(abs(fftshift(1./err_vec)), 'r'); hold off
-%figure; plot(abs(fftshift(rx_sym)));
+%figure; hold on ; plot(abs(sig_in)); plot(abs(1./err_vec_preamble), 'r'); hold off
+
+%t = sig_in; t(X) = 0;
+%figure; axes('YScale','log'); hold on ; plot(abs(t)); plot(abs(1./err_vec_preamble), 'r'); hold off
+%ylim([1e3 1e6]);
+%title('Noise (blue) and channel profile (red)');
+%clear t;
+
+%figure; semilogy(abs(err_vec_preamble)) ; grid on ;
 %scatterplot(rx_sym);
+
+%% Calculate SNR
+t = rx_sym; t(X)=0; t = t(86:1024-86);
+%figure; plot(abs(rx_sym(86:1024-86)));
+%figure; plot(abs(t));
+snr = mean(t);
+fprintf('SNR in preamble = %.1f dB\n', db(snr));
+clear t;
+
+%% Clear workspace
+clear X sp_ref sp_ref sp_rx sp_err sig_in rx_sym;
