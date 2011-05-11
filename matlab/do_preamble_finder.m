1d8ef8d63a93b54016fb320c095edde9bc151321
Alexander Chemeris <chemeris@users.noreply.github.com>
2011-05-11 21:38:37 +0400

    Matlab: Move subchannels extraction for a PDU block to a separate function PDU_extract()

--- matlab/do_preamble_finder.m
+++ matlab/do_preamble_finder.m
@@ -125,48 +125,15 @@ if DL_Map_Repetition == 0
 else
     DL_Map_Repetition = 2*DL_Map_Repetition;
 end
+DL_MAP_length_sym = ceil(DL_Map_Length/params.N_subchannels);
 
-%% Extracted and averaged QPSK characters that contain DL MAP
-dl_map_qpsk = zeros(1, 48*DL_Map_Length/DL_Map_Repetition);     
-k = 1; 
-count_slots = 4; % first 4 slots for FCH
-j = count_slots + 10*params.segment; % Index of first slot DL-MAP (first slot after FCH)
-t = zeros(DL_Map_Repetition, 48); 
-t_index = 1; 
-first_sym = 1; 
-while k<=DL_Map_Length/DL_Map_Repetition
-    % Collect DL_Map_Repetition of slots
-    t(t_index, :) = get_slot_data(syms_fft_eq(first_sym:first_sym+1,:), j, 1, 2, params);
-    if t_index==DL_Map_Repetition
-         t_index = 1; 
-         if DL_Map_Repetition ~= 1
-             % Average all repetitions
-             dl_map_qpsk(1+(k-1)*48: k*48) = sum(t);
-         else
-             % Save the only repetition
-             dl_map_qpsk(1+(k-1)*48: k*48) = t; 
-         end
-         k = k+1;              
-    else
-        t_index = t_index+1; 
-    end
-    if j==29 % The magic value "29" is index of the last slot
-      % The last slot of ofdm symbols pair 
-      j = 0; 
-    else
-      % Ajust number of the slot
-      j = j+1; 
-    end
-    
-    if  count_slots == 29     
-        count_slots = 0; 
-        first_sym = first_sym+2;         
-    else
-        count_slots = count_slots+1;
-    end
-          
-end
-
+%% Extract DL-MAP 
+% DL-MAP is the first slot after FCH, so we should skip 4 subchannels
+% of FCH
+dl_map_qpsk = PDU_extract(DL_Map_Repetition, 4, DL_Map_Length, ...
+                          1, 0, ... start: OFDM symbol 1, subchanel 0
+                          DL_MAP_length_sym, params.N_subchannels, ...
+                          syms_fft_eq, params);
 figure(11); 
 plot(dl_map_qpsk, 'o'), title('averaged repetitions of DL_MAP'); 
  
