f64e6e9674529634c60bd69bea6f10550e82f0f5
Alexander Chemeris <chemeris@users.noreply.github.com>
2011-05-11 21:40:54 +0400

    Matlab: Add license header to do_preamble_finder.m

--- matlab/do_preamble_finder.m
+++ matlab/do_preamble_finder.m
@@ -1,4 +1,26 @@
-% main script of testbench OFDM system 
+% The main script of WiMAX receiver
+%
+% This script relies on data produced by the main script and on an array of
+% broadcast block positions from 'diuc_o.txt' file.
+
+% Copyright (C) 2011  Alexander Chemeris
+%
+% This library is free software; you can redistribute it and/or
+% modify it under the terms of the GNU Lesser General Public
+% License as published by the Free Software Foundation; either
+% version 2.1 of the License, or (at your option) any later version.
+%
+% This library is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+% Lesser General Public License for more details.
+%
+% You should have received a copy of the GNU Lesser General Public
+% License along with this library; if not, write to the Free Software
+% Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+% USA
+
+%% Prepare a bench
 clear all
 set_params
 CTC_set_params
