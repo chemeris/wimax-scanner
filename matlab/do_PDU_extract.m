f34f6fead96e43528326d22e58de82b65ee272be
Alexander Chemeris <chemeris@users.noreply.github.com>
2011-08-25 02:47:21 +0400

    matlab: Update do_PDU_extract.m to decode both DIUC 0 and DIUC 1.

--- matlab/do_PDU_extract.m
+++ matlab/do_PDU_extract.m
@@ -1,7 +1,7 @@
 % Extract broadcast blocks from a set of frames.
 %
 % This script relies on data produced by the main script and on an array of
-% broadcast block positions from 'diuc_o.txt' file.
+% broadcast block positions from 'diuc_list.txt' file.
 
 % Copyright (C) 2011  Alexander Chemeris
 %
@@ -20,23 +20,43 @@
 % Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
 % USA
 
+%% Load DIUC coding parameters
+% Note: Modify this to reflect your base station settings.
+diuc_coding = { ...
+    'QPSK_1/2'   ... DIUC 0
+    'QPSK_3/4'   ... DIUC 1
+    '16-QAM_1/2' ... DIUC 2
+    '16-QAM_3/4' ... DIUC 3
+    '64-QAM_1/2' ... DIUC 4
+    '64-QAM_2/3' ... DIUC 5
+    '64-QAM_3/4' ... DIUC 6
+    '64-QAM_5/6' ... DIUC 7
+    };
+
 %% Load positions of broadcast segments
-diuc_0 = load('diuc_0.txt','-ascii');
+diuc_list = load('diuc_list.txt','-ascii');
 
 %% Cycle trough all the data
-frames_total = length(diuc_0(:,1));
+frames_total = length(diuc_list(:,1));
 for k = 1:frames_total
     %% Get segment information
-    frame_num = diuc_0(k,1);
-    PDU_start_sym = diuc_0(k,2);
-    PDU_start_subch = diuc_0(k,3);
-    PDU_width_sym = diuc_0(k,4);
-    PDU_width_subch = diuc_0(k,5);
-    PDU_repetition = diuc_0(k,6);
+    frame_num = diuc_list(k,1);
+    PDU_diuc = diuc_list(k,2);
+    PDU_start_sym = diuc_list(k,3);
+    PDU_start_subch = diuc_list(k,4);
+    PDU_width_sym = diuc_list(k,5);
+    PDU_width_subch = diuc_list(k,6);
+    PDU_repetition = diuc_list(k,7);
 
-    fprintf('%d | Start: %d sym, %d subch | Size: %d sym x %d subch | Repetition: %d ', ...
-            frame_num, PDU_start_sym, PDU_start_subch, PDU_width_sym, PDU_width_subch, ...
-            PDU_repetition);
+    %% Higher order modulation is not implemented yet
+    if PDU_diuc > 1
+        continue;
+    end
+    
+    %% Initial information output
+    fprintf('#%d DIUC %d | Start: %d sym, %d subch | Size: %d sym x %d subch | Repetition: %d ', ...
+            frame_num, PDU_diuc, PDU_start_sym, PDU_start_subch, ...
+            PDU_width_sym, PDU_width_subch, PDU_repetition);
 
 
     %% Demodulate enough OFDM symbols
@@ -70,17 +90,21 @@ for k = 1:frames_total
        PDU_qpsk = sum(PDU_qpsk);
     end
     %% Decode
-    [info, ~, number_of_errors_in_PDU] = CTC_Decode_Blocks(PDU_qpsk, 'QPSK_1/2', CTC_params );
-    fprintf(' PDU error bits = %d\n',  number_of_errors_in_PDU);
+    [info, ~, number_of_errors_in_PDU] = CTC_Decode_Blocks(PDU_qpsk, diuc_coding(PDU_diuc+1), CTC_params );
+    fprintf(' PDU error bits = %d of %d (%.0f%%)\n', ...
+            number_of_errors_in_PDU, length(info), 100*number_of_errors_in_PDU/length(info));
 
     %% Output to a file
-    % Output only if we have reasonable amount of errors.
-    if number_of_errors_in_PDU < 50
+    % Output all data - it will be checed with CRC later anyway.
+    if 1
         fid = fopen('bit.txt', 'a');
         fprintf(fid, '%d PDU %02d ', frame_num, PDU_width_sym*PDU_width_subch/2);
         fprintf(fid, '%d', info);
         fprintf(fid, '\n');
         fclose(fid);
     end
+    
+    %% Pause to allow user see figures
+    %pause(5);
 end
 
