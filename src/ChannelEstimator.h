4511153da9b6d3293f79a4f02e9e43cd02d3d030
Alexander Chemeris <chemeris@users.noreply.github.com>
2012-04-13 18:34:52 +0000

    Add documentation for the channel estimator.

--- src/ChannelEstimator.h
+++ src/ChannelEstimator.h
@@ -4,6 +4,18 @@
 #include "wimax_consts.h"
 #include "complex.h"
 
+/**
+* OFDM Channel Estimator.
+*
+* Channel estimator has very simple structure and works like many other
+* OFDM estimators do:
+*  1. Estimations from OFDM pilots are smothed and then.
+*  2. Smoother estimations are interpolated to non-pilot sub-carriers
+*     by convolution with the Hamming window.
+*  3. Interpolated data is filtered in time (i.e. smoothed over OFDM symbols)
+*     with a filter like y(n) = (1-k)*y(n-1) + k*x(k), where n is an OFDM symbol,
+*     and k is some number < 1.
+*/
 class tChannelEstimator
 {
 	public:
